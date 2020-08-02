d1dd66910e5d1e8fd621d1fcae39413d6c0c5112
Shanthi Kolakani <shanthi.kolakani@zensar.com>
2020-08-03 03:51:35 +0530

    add new file to master

---
+++ GameInheritenceApp/GameInheritenceApp/Game.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameInheritenceApp
+{
+    class Game
+    {
+        public int noOfplayers;
+        public int Country;
+        public string Coachname;
+        private int now;
+  
+        public Game()
+        {
+
+        }
+        public Game(int _noOfplayers, int _country,string _coachName)
+        {
+            noOfplayers = _noOfplayers;
+            Country = _country;
+            Coachname = _coachName;
+
+        }
+        static void WorldCup()
+        { 
+            DateTime dt = new DateTime(2007); 
+            Console.WriteLine(dt);
+
+        }
+
+        public void Display()
+        {
+            Console.WriteLine(" details");
+        }
+
+
+    }
+
+    class Cricket : Game
+    {
+     
+
+        public Cricket()
+        {
+
+        }
+        public Cricket(int _noofplayers, int _country, string _coachname) :base(_noofplayers, _country, _coachname)
+        {
+            noOfplayers = _noofplayers;
+            Country = _country;
+            Coachname = _coachname;
+        }
+        public override void ShowCricket()
+        {
+            Console.WriteLine("Cricket details");
+        }
+    }
+     class FootBall : Game
+    {
+        
+        public int noofPlayers;
+        public string LeagueName;
+        public int counTry;
+
+        public FootBall()
+        {
+
+        }
+       public  FootBall(int _noofPlayers, string _leagueName, int _counTry) : base(_noofPlayers,_counTry, _leagueName)
+        {
+            noofPlayers = _noofPlayers;
+            LeagueName = _leagueName;
+            counTry = _counTry;
+        }
+        static void ShowFootBall()
+        {
+            Console.WriteLine("football details");
+        }
+
+    }
+   
+
+    class Program 
+    {
+        static void Main()
+        {
+            Console.WriteLine("------------------CricketInformation----------------");
+            Cricket India = new Cricket();
+            Cricket Australia = new Cricket();
+             India.
+
+
+
+            Console.WriteLine("------------------FootBallInformation----------------");
+            FootBall Spain = new FootBall();
+            FootBall England = new FootBall();
+
+            Game g = new Game();
+
+
+
+        }
+    }
+}
---
+++ QuadrilateralApp/QuadrilateralApp/AbstractClassQuadlat.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuadrilateralApp;
+
+namespace QuadrilateralApp
+{
+    abstract class Quadrilateral
+
+    {
+        public int isSquare;
+        public int area;
+      public int LengthOfSquare = 4;
+    public int LengthOfRec = 5;
+    public  int BreathOfRec = 6;
+        public Quadrilateral()
+        {
+
+        }
+        public Quadrilateral(int _lengthOfSquare)
+        {
+            _lengthOfSquare = LengthOfSquare;
+        }
+        public Quadrilateral(int _lengthOfRec, int _breathOfRec)
+        {
+
+            _lengthOfRec = LengthOfRec;
+            _breathOfRec = BreathOfRec;
+
+        }
+
+        public void Display()
+        {
+
+        }
+        public abstract string ShowArea();
+
+
+
+    }
+
+    class Square : Quadrilateral
+    {
+        public Square()
+        {
+
+        }
+        public Square(int _lengthOfSqu) : base(_lengthOfSqu)
+        {
+
+        }
+
+        public int Area(int _lengthOfSquare)
+        {
+            area = _lengthOfSquare;
+            return area;
+        }
+
+        public override string ShowArea()
+        {
+            return "Square is greater than Rectangle area";
+        }
+
+    }
+
+    class Rectangle : Quadrilateral
+    {
+        public Rectangle()
+        {
+
+        }
+        public Rectangle(int _lengthOfRec, int _breathOfRec) : base(_lengthOfRec, _breathOfRec)
+        {
+            BreathOfRec = _breathOfRec;
+        }
+
+        public int IsSquare
+        {
+            get { return BreathOfRec; }
+            set
+            {
+                if(value == LengthOfRec)
+                {
+                    Console.WriteLine("It is a Square");
+                }
+                else
+                {
+                    BreathOfRec = value;
+                }
+
+            }
+        }
+        public int Area(int _lengthOfRec, int _breathOfRec)
+        {
+            area = _lengthOfRec * _breathOfRec;
+            return area;
+        }
+        public override string ShowArea()
+        {
+
+            return "Rectangle is greater";
+        }
+
+
+    }
+
+
+    class AbstractClassQuadlat
+    {
+        static void Main()
+        {
+            Quadrilateral r = new Rectangle();
+            r.area = Convert.ToInt32(Console.ReadLine());
+            r.ShowArea();
+            r.Display();
+
+           
+            Quadrilateral s = new Square();
+            s.area = Convert.ToInt32(Console.ReadLine());
+            r.ShowArea();
+            r.Display();
+
+            Rectangle i = new Rectangle();
+           
+
+            /*f (s.area > r.area)
+             {
+                 Console.WriteLine("Square is greater than Rectangle area");
+             }
+             else {
+                 Console.WriteLine("Rectangle is greater than Square");
+             }
+            */
+            Console.WriteLine("{0} {1}", r.area, s.area);
+            Console.Read();
+            
+            
+            
+       
+        }
+    }
+}
